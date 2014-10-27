ae1a654263626c0e648ee07d1efd7f1f80705ad6
Courtney de Lautour <clautour@thefrontiergroup.com.au>
2014-10-27 18:15:32 +0800

    Add tests

---
+++ spec/rest/it_works_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'dojo_rest'
+
+describe "test rspec" do
+ it { expect(DojoRest::VERSION).to eq "0.0.1"}
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'rspec'
+
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
